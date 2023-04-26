0d40dcbd3a5ad4fc433328170fddea46d41ddcf6
Prajakta Jakhadi <prajakta_jakhadi@epam.com>
2023-04-26 12:59:28 +0530

    added new branch and class

---
+++ src/JavaPrograms/fourthProgram.java
@@ -0,0 +1,10 @@
+package JavaPrograms;
+
+public class fourthProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("fourth program");
+	}
+
+}
